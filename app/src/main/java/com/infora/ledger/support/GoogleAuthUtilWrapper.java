a1721d674754a4d8596e0f4ffb43f4852f499452
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-01 23:32:15 +0300

    Adjusting build script to set google api client_id from special (stored local only) properties file. Adding more logging.

--- app/src/main/java/com/infora/ledger/support/GoogleAuthUtilWrapper.java
+++ app/src/main/java/com/infora/ledger/support/GoogleAuthUtilWrapper.java
@@ -1,9 +1,11 @@
 package com.infora.ledger.support;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
+import com.infora.ledger.BuildConfig;
 
 import java.io.IOException;
 
@@ -11,12 +13,19 @@ import java.io.IOException;
  * Created by jenya on 21.03.15.
  */
 public class GoogleAuthUtilWrapper {
-    private static final String ID_TOKEN_SCOPE = "audience:server:client_id:127152602937-l6sn5g0albld900plkhh4b7fdjqee620.apps.googleusercontent.com";
+    private static final String TAG = GoogleAuthUtilWrapper.class.getName();
+
+    private static final String ID_TOKEN_SCOPE = "audience:server:client_id:" + BuildConfig.GOAUTH_API_CLIENT_ID;
     public String getToken(Context context, String accountName, boolean invalidate) throws GoogleAuthException, IOException {
+        Log.d(TAG, "Retrieving the id_token.");
         String token = GoogleAuthUtil.getToken(context, accountName, ID_TOKEN_SCOPE);
-        if(invalidate) {
+        if (invalidate) {
+            Log.d(TAG, "Invalidate option supplied. Invalidating the token and getting a new one.");
             GoogleAuthUtil.invalidateToken(context, token);
-            token = GoogleAuthUtil.getToken(context, accountName, ID_TOKEN_SCOPE);
+            String newToken = GoogleAuthUtil.getToken(context, accountName, ID_TOKEN_SCOPE);
+            if(newToken == token)
+                throw new GoogleAuthException("Failed to invalidate the token. Same token retrieved.");
+            token = newToken;
         }
         return token;
     }
