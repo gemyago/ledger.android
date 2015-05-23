b5faeef34ec8f0d028367bff60df99524e494964
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-05-23 22:42:05 +0300

    Implementing logic to build signature.

---
+++ app/src/androidTest/java/com/infora/ledger/banks/DefaultPrivatBankRequestSignatureBuilderTest.java
@@ -0,0 +1,22 @@
+package com.infora.ledger.banks;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * Created by jenya on 23.05.15.
+ */
+public class DefaultPrivatBankRequestSignatureBuilderTest extends TestCase {
+
+    public void testBuild() throws Exception {
+        String data = "some-fake-data";
+        String password = "some-fake-password";
+
+        String sha1 = new String(Hex.encodeHex(DigestUtils.sha1(new String(Hex.encodeHex(DigestUtils.md5(data + password))))));
+
+        DefaultPrivatBankRequestSignatureBuilder subject = new DefaultPrivatBankRequestSignatureBuilder();
+        assertEquals(sha1, subject.build(data, password));
+    }
+}
--- app/src/main/java/com/infora/ledger/banks/DefaultPrivatBankRequestSignatureBuilder.java
+++ app/src/main/java/com/infora/ledger/banks/DefaultPrivatBankRequestSignatureBuilder.java
@@ -1,11 +1,16 @@
 package com.infora.ledger.banks;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  * Created by jenya on 23.05.15.
  */
 public class DefaultPrivatBankRequestSignatureBuilder implements PrivatBankRequestSignatureBuilder {
     @Override
     public String build(String data, String password) {
-        return null;
+        byte[] md5 = DigestUtils.md5(data + password);
+        byte[] sha1 = DigestUtils.sha1(new String(Hex.encodeHex(md5)));
+        return new String(Hex.encodeHex(sha1));
     }
 }
