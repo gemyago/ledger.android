80620fd9f47845dde9aa964556832d961f9efc54
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-05-30 22:41:56 +0300

    Enabling back navigation for the settings activity.

--- app/src/main/java/com/infora/ledger/SettingsActivity.java
+++ app/src/main/java/com/infora/ledger/SettingsActivity.java
@@ -11,5 +11,6 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
