6ed33c2677a9aa45e43a7c5a2b3498d2de9495bc
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-05-30 22:29:31 +0300

    Adding action bar to the settings activity.

--- app/src/main/java/com/infora/ledger/SettingsActivity.java
+++ app/src/main/java/com/infora/ledger/SettingsActivity.java
@@ -1,18 +1,15 @@
 package com.infora.ledger;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by jenya on 27.03.15.
  */
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.activity_settings);
     }
 }
