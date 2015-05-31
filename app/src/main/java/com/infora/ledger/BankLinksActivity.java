48cb786e6f4ba9b2cb44a006d2a5ef206a8e5068
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-05-31 10:27:28 +0300

    Moving add action to the action bar.

--- app/src/main/java/com/infora/ledger/BankLinksActivity.java
+++ app/src/main/java/com/infora/ledger/BankLinksActivity.java
@@ -2,6 +2,7 @@ package com.infora.ledger;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 
 /**
  * Created by jenya on 30.05.15.
@@ -12,5 +13,12 @@ public class BankLinksActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_links);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bank_links, menu);
+        return true;
     }
 }
