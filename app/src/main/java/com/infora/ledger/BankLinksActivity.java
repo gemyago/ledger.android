69b8ac8fac02e7ab48d77de1551f9d52a531638b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-06-01 13:36:34 +0300

    Adding logic to show bank links.

--- app/src/main/java/com/infora/ledger/BankLinksActivity.java
+++ app/src/main/java/com/infora/ledger/BankLinksActivity.java
@@ -1,20 +1,24 @@
 package com.infora.ledger;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-import com.infora.ledger.data.BankLink;
-import com.infora.ledger.support.BusUtils;
+import com.infora.ledger.BanksContract.BankLinks;
 
 /**
  * Created by jenya on 30.05.15.
  */
-public class BankLinksActivity extends ActionBarActivity {
+public class BankLinksActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int BANK_LINKS_LOADER_ID = 1;
     private SimpleCursorAdapter bankLinksAdapter;
     private ListView lvBankLinks;
 
@@ -24,13 +28,16 @@ public class BankLinksActivity extends ActionBarActivity {
         setContentView(R.layout.activity_bank_links);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        bankLinksAdapter = new SimpleCursorAdapter(this, R.layout.transactions_list,
+        bankLinksAdapter = new SimpleCursorAdapter(this, R.layout.banks_links_list,
                 null,
-                new String[]{BanksContract.BankLinks.COLUMN_BIC},
-                new int[]{R.id.bank_link_data}, 0);
+                new String[]{BankLinks.COLUMN_BIC, BankLinks.COLUMN_ACCOUNT_NAME},
+                new int[]{R.id.bank_link_data, R.id.ledger_account_name}, 0);
+
         lvBankLinks = (ListView) findViewById(R.id.bank_links_list);
         lvBankLinks.setAdapter(bankLinksAdapter);
         lvBankLinks.setEmptyView(findViewById(android.R.id.empty));
+
+        getLoaderManager().initLoader(BANK_LINKS_LOADER_ID, null, this);
     }
 
     @Override
@@ -49,7 +56,25 @@ public class BankLinksActivity extends ActionBarActivity {
             return true;
         }
 
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (BANK_LINKS_LOADER_ID == id) {
+            return new CursorLoader(this, BankLinks.CONTENT_URI, null, null, null, null);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        bankLinksAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        bankLinksAdapter.swapCursor(null);
     }
 }
