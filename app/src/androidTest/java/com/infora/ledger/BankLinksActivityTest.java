2f7486f048df706a68742934812c3bfea702d6ca
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-06-01 16:11:41 +0300

    Code cleanup.

--- app/src/androidTest/java/com/infora/ledger/BankLinksActivityTest.java
+++ app/src/androidTest/java/com/infora/ledger/BankLinksActivityTest.java
@@ -3,6 +3,7 @@ package com.infora.ledger;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.test.RenamingDelegatingContext;
 import android.widget.ListView;
 
 import com.infora.ledger.application.commands.DeleteBankLinksCommand;
@@ -41,6 +42,8 @@ public class BankLinksActivityTest extends android.test.ActivityUnitTestCase<Ban
         bus = new EventBus();
         startActivity(new Intent(), null, null);
         BusUtils.setBus(getActivity(), bus);
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getActivity(), "-bank-links-activity-test");
+        setActivityContext(context);
 
         repo = new BankLinksRepository(getActivity());
         DbUtils.deleteAllDatabases(getActivity());
