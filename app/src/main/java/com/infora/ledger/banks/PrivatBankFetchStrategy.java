aeb28b37f2d776d94b73916052a7a42f4e108c67
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-06-09 17:03:58 +0300

    Adding fetch algorithm description.

--- app/src/main/java/com/infora/ledger/banks/PrivatBankFetchStrategy.java
+++ app/src/main/java/com/infora/ledger/banks/PrivatBankFetchStrategy.java
@@ -17,10 +17,17 @@ public class PrivatBankFetchStrategy extends FetchStrategy {
 
     @Override
     public void fetchBankTransactions(DatabaseContext db, BankLink bankLink) {
-
-    }
-
-    private boolean isToday(Date date) {
-        return false;
+        /* TODO
+        set flags (inProgress=true and hasSucceed=false)
+        starting from (exclusive) lastSyncDate till todayDate
+        * if lastSyncDate is todayDate then fetch todays transactions again
+        use api to fetch transactions
+        use transactions read model(TODO) to get actual transactions for given bic
+        start unit of work
+        insert new transactions (that have not yet been saved)
+        update flags (inProgress=false hasSucceed=true)
+        update lastSyncDate to now
+        commit unit of work
+        * */
     }
 }
