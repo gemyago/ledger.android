a8ed0570c12f11f7d74e57b02a78d9fb162330dd
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-07-27 19:19:09 +0300

    Adding custom preference to select default account.

---
+++ app/src/main/java/com/infora/ledger/DefaultTransactionAccountPreference.java
@@ -0,0 +1,76 @@
+package com.infora.ledger;
+
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+
+import com.infora.ledger.data.LedgerAccountsLoader;
+
+/**
+ * Created by mye on 7/27/2015.
+ */
+public class DefaultTransactionAccountPreference extends DialogPreference {
+    private static final String TAG = DefaultTransactionAccountPreference.class.getName();
+
+    private static final int LEDGER_ACCOUNTS_LOADER = 1;
+    private SimpleCursorAdapter accountsAdapter;
+    private Spinner ledgerAccountId;
+
+    public DefaultTransactionAccountPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setDialogLayoutResource(R.layout.default_ledger_account_preference);
+        setPositiveButtonText(android.R.string.ok);
+        setNegativeButtonText(android.R.string.cancel);
+
+        setDialogIcon(null);
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        View view = super.onCreateDialogView();
+        ledgerAccountId = (Spinner) view.findViewById(R.id.ledger_account_id);
+
+        accountsAdapter = new SimpleCursorAdapter(getContext(),
+                android.R.layout.simple_spinner_item,
+                null,
+                new String[]{LedgerAccountsLoader.COLUMN_NAME},
+                new int[]{android.R.id.text1},
+                0);
+        ledgerAccountId.setAdapter(accountsAdapter);
+        Activity activity = (Activity) getContext();
+
+        LoaderManager loaderManager = activity.getLoaderManager();
+
+        Log.d(TAG, "Initializing accounts loader");
+        loaderManager.initLoader(LEDGER_ACCOUNTS_LOADER, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+            @Override
+            public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                Log.d(TAG, "Creating the loader");
+                return new LedgerAccountsLoader(getContext()).withSelectionPrompt();
+            }
+
+            @Override
+            public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+                Log.d(TAG, "Loading finished.");
+                accountsAdapter.swapCursor(data);
+            }
+
+            @Override
+            public void onLoaderReset(Loader<Cursor> loader) {
+                accountsAdapter.swapCursor(null);
+            }
+        });
+
+        return view;
+    }
+}
