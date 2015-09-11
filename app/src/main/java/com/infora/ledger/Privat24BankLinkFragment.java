f2fb7e7442fbc3f76c81b8b8cd25ee114131e066
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-09-11 17:17:50 +0300

    Adding logic to prompt for the OTP password.

--- app/src/main/java/com/infora/ledger/Privat24BankLinkFragment.java
+++ app/src/main/java/com/infora/ledger/Privat24BankLinkFragment.java
@@ -1,7 +1,12 @@
 package com.infora.ledger;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,19 +14,49 @@ import android.widget.EditText;
 
 import com.infora.ledger.api.DeviceSecret;
 import com.infora.ledger.banks.ua.privatbank.Privat24BankLinkData;
-import com.infora.ledger.banks.ua.privatbank.PrivatBankLinkData;
+import com.infora.ledger.banks.ua.privatbank.messages.AskPrivat24Otp;
+import com.infora.ledger.banks.ua.privatbank.messages.AuthenticateWithOtp;
+import com.infora.ledger.banks.ua.privatbank.messages.CancelAddingBankLink;
 import com.infora.ledger.data.BankLink;
+import com.infora.ledger.support.BusUtils;
 import com.infora.ledger.ui.BankLinkFragment;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by jenya on 01.06.15.
  */
 public class Privat24BankLinkFragment extends BankLinkFragment<Privat24BankLinkData> {
 
+    private static final String TAG = Privat24BankLinkFragment.class.getName();
+
+    @Override
+    public void onBeforeAdd(Activity parent) {
+        EventBus bus = BusUtils.getBus(parent);
+        bus.register(this);
+        Log.d(TAG, "Fragment registered to handle events.");
+    }
+
+    @Override
+    public void onBeforeRemove(Activity parent) {
+        EventBus bus = BusUtils.getBus(parent);
+        bus.unregister(this);
+        Log.d(TAG, "Fragment unregistered from events handling.");
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_privat24_bank_link, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        EventBus bus = BusUtils.getBus(getActivity());
+        bus.unregister(this);
+        Log.d(TAG, "Fragment unregistered from events handling.");
     }
 
     @Override
@@ -54,5 +89,30 @@ public class Privat24BankLinkFragment extends BankLinkFragment<Privat24BankLinkD
         login.setText("");
         password.setText("");
         card.setText("");
+    }
+
+    public void onEventMainThread(final AskPrivat24Otp cmd) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Please provide OTP password");
+
+        final EditText input = new EditText(getActivity());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String otpPassword = input.getText().toString();
+                BusUtils.post(getActivity(), new AuthenticateWithOtp(cmd.linkId, cmd.operationId, otpPassword));
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                BusUtils.post(getActivity(), new CancelAddingBankLink(cmd.linkId));
+            }
+        });
+
+        builder.show();
     }
 }
