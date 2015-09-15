6fb5fbd9db8c052b4c587ed28d69ffe023297c30
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-09-15 18:30:01 +0300

    Code cleanup.

--- app/src/main/java/com/infora/ledger/ReportActivity.java
+++ app/src/main/java/com/infora/ledger/ReportActivity.java
@@ -135,12 +135,8 @@ public class ReportActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
@@ -155,8 +151,6 @@ public class ReportActivity extends ActionBarActivity {
             startActivity(new Intent(this, BankLinksActivity.class));
             return true;
         }
-
-
         return super.onOptionsItemSelected(item);
     }
 
