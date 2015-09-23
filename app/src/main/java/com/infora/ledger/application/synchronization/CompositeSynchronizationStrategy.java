6c09c3843c291374c4c2aea33d2a69c92119ee0b
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-09-23 16:30:31 +0300

    Adding composite strategy.

---
+++ app/src/main/java/com/infora/ledger/application/synchronization/CompositeSynchronizationStrategy.java
@@ -0,0 +1,23 @@
+package com.infora.ledger.application.synchronization;
+
+import android.accounts.Account;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by mye on 9/23/2015.
+ */
+public class CompositeSynchronizationStrategy implements SynchronizationStrategy {
+    private SynchronizationStrategy[] strategies;
+
+    public CompositeSynchronizationStrategy(SynchronizationStrategy... strategies) {
+        this.strategies = strategies;
+    }
+
+    @Override
+    public void synchronize(Account account, Bundle options, SyncResult syncResult) throws SynchronizationException {
+        for (SynchronizationStrategy strategy : strategies) {
+            strategy.synchronize(account, options, syncResult);
+        }
+    }
+}
