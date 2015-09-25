434b695d0362e16dcdc5f4e6c7b4506ec86364a6
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-09-25 14:24:19 +0300

    Correcting deprecation.

--- app/src/main/java/com/infora/ledger/SettingsActivity.java
+++ app/src/main/java/com/infora/ledger/SettingsActivity.java
@@ -2,11 +2,12 @@ package com.infora.ledger;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by jenya on 27.03.15.
  */
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
